8600e912d10a2ceb596061d202130366f414635d
Daniela <nam991019@gmail.com>
2024-11-04 18:31:10 -0500

    console

--- script.js
+++ script.js
@@ -4,5 +4,14 @@ let numero;
 do {
     numero =parseInt(prompt("Ingresa un numero (ingresa 0000 para finalizar): "));
 
-    if (numero !== 0000);
-}
+    if (numero !== 0) {
+        suma += numero;
+    }
+
+} while (numero !== 0000);
+
+console.log ("El valor acumulado es: " + suma);
+
+if(suma > 0) {
+    console.log("El valor acumulado es mayor a cero. ");
+} else if (suma < 0)
